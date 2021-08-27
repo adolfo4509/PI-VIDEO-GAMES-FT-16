74945dac62adc9299448accb8db655b21589553a
adolfo4509 <calvo330906@gmail.com>
2021-08-27 14:36:25 -0500

    Modelos-terminados

---
+++ PI-Videogames/api/src/models/Genders.js
@@ -0,0 +1,7 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("gender", {
+    name: { type: DataTypes.STRING, allowNull: false },
+  });
+};
--- PI-Videogames/api/src/models/Videogame.js
+++ PI-Videogames/api/src/models/Videogame.js
@@ -1,12 +1,15 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+  sequelize.define("videogame", {
+    id: { type: DataTypes.UUID, allowNull: false, defaultValue: UUIDV4 },
+    name: { type: DataTypes.STRING, allowNull: false },
+    genres: { type: DataTypes.STRING, allowNull: false },
+    platforms: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.STRING, allowNull: true },
+    rating: { type: DataTypes.STRING, allowNull: true },
+    released: { type: DataTypes.STRING, allowNull: true },
   });
 };
