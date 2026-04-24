ffef583301182e44c9fd742acc59b3c684a37813
adolfo <calvo330906@gmail.com>
2026-04-24 11:30:04 -0500

    feat: Se agregan correciones y se actualiza node

--- PI-Videogames/client/src/Constantes/Constans.js
+++ PI-Videogames/client/src/Constantes/Constans.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "http://localhost:3001/api/";
 export const BASE_PLATAFORMS = BASE_URL + "platforms/plataforms";
-export const VIDEOGAMES_URL = BASE_URL + "videogames/videogames";
+export const VIDEOGAMES_URL = BASE_URL + "videogames";
 export const VIDEOGAMENAME_URL = BASE_URL + "videogames/videogame/";
 export const VIDEOGAMEID_URL = BASE_URL + "videogames/videogames/";
 export const VIDEOGAME_CREATE_URL = BASE_URL + "videogames/videogame";
